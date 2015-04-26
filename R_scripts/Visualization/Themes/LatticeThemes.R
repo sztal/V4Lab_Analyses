fa792844a4bd1bbfaf06c36b6dc317ea9cf566c4
sztal <sztal111@gmail.com>
2015-04-26 20:56:10 +0200

    Add directory with helper script for data visualization

---
+++ R_scripts/Visualization/Themes/LatticeThemes.R
@@ -0,0 +1,32 @@
+################################################################
+### Custom themes for visualization with the lattice package ###
+################################################################
+
+### Themes are realized as functions.
+### All of them are used in the same manner:
+### one has to call them like this: theme <- theme_function(theme_name)
+### then the function creates an object of the name theme
+### and assigns a particular theme to it.
+### For a catalogue of themes to choose see the function's body
+### Any further customization to a theme may be either done directly on the object
+### or by altering the code here and calling the function once again.
+
+### !!! <--- Theme 1 ---> !!! ###
+### Standard black-gray-white V4 Lab theme
+V4themes <- function(theme_name) {
+      ### Check the input data
+      stopifnot(is.character(theme_name) & length(theme_name) == 1)
+      choices <- c("standard_bgw")
+      theme_name <- match.arg(theme_name, choices = choices)
+      stopifnot(theme_name %in% choices)
+      
+      V4.theme <- trellis.par.get()
+      ### Choose a theme
+      if(theme_name == "standard_bgw") {
+            ### Standard black-gray-white theme; very good for paper publications
+            V4.theme$strip.background$col <- "gray"
+            V4.theme$plot.polygon$col <- "gray3"
+            V4.theme$plot.polygon$border <- "transparent"
+      }
+      return(V4.theme)
+}
